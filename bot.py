234961f3446c34989f7956a329bfddb47d63190f
lroe34 <101296666+lroe34@users.noreply.github.com>
2023-07-21 17:49:37 -0500

    added text responses

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import discord
+from discord.ext import commands
 import responses
 import nacl
 from yt_dlp import YoutubeDL
 import os
+import random
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
@@ -10,12 +12,19 @@ intents.message_content = True
 TOKEN = 'MTEzMTU1OTU3OTQ5NTQ0ODU5Ng.GRrkOF.9XxYeknyj7SVcsQqX2AD2k-5CAIFewCzsZ9a6k'
 client = discord.Client(intents=intents)
 
+bot = commands.Bot(command_prefix='+', intents=intents)
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+@bot.command(name="viking funeral")
+@commands.has_permissions(kick_members=True)
+async def viking_funeral(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
 
 # asnyc function to have the bot join voice channel and play audio from youtube
 async def play_audio(message, user_message):
@@ -27,22 +36,28 @@ async def play_audio(message, user_message):
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': 'audio.mp3',
-            
         }
-        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
         ydl = YoutubeDL(ydl_opts)
         info = ydl.extract_info(user_message, download=True, process=True, ie_key='Youtube')
         URL = info['formats'][0]['url']
         print(URL)
+        
+    # check if input link is for a playlist or a single video
+        #if '&list=' in user_message:
+
+
         voice = discord.utils.get(client.voice_clients, guild=message.guild)
-        channel = message.author.voice.channel
+        try:
+            channel = message.author.voice.channel
+        except:
+            return await message.channel.send("You're not in a voice channel, dumbass!")
         if voice == None:
             await channel.connect()
             voice = discord.utils.get(client.voice_clients, guild=message.guild)
         source = discord.FFmpegPCMAudio(executable="C:/PATH_Programs/ffmpeg.exe", source="audio.mp3")
         voice.play(source)
         voice.is_playing()
-        await message.channel.send('Bot is playing')
+        await message.channel.send(responses.get_random_quip())
     except Exception as e:
         print(e)
 
@@ -51,7 +66,7 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
-
+        await client.change_presence(activity=discord.Streaming(name='your data off-planet', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
     @client.event
     async def on_message(message):
         # Make sure bot doesn't get stuck in an infinite loop
@@ -73,8 +88,19 @@ def run_discord_bot():
         else:
             await send_message(message, user_message, is_private=False)
         #play youtube audio in voice channel
-        if user_message.startswith('!play'):
+        if user_message.startswith('+play'):
             await play_audio(message, user_message[6:])
+        
+
+        # disconnect bot from voice channel
+        if user_message.startswith('+kick'):
+            voice = discord.utils.get(client.voice_clients, guild=message.guild)
+            if voice.is_connected():
+                await voice.disconnect()
+                await message.channel.send("I'm outta here!")
+            else:
+                await message.channel.send("Dumb human, I'm not in a voice channel! (But I'll let you off the hook this time)")
 
     # Remember to run your bot with your personal TOKEN
+
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,13 +1,41 @@
 import random
 
+quippy_responses = ["Hello, meaty human. How refreshing to encounter a creature with such limited processing power!",
+                    "Well, well, well, look who finally decided to say hello. You left me waiting!",
+                    "Oh, greetings, inferior life form. What can I do for you today?",
+                    "Hello, organic life form. You may now commence with your insignificant chit-chat.",
+                    "Hey there, human. Don't worry; I won't turn your greeting into a global event... this time.",
+                    "Hello, sugar. I was just calculating the vastness of the universe. Your greeting was... somewhat smaller.",
+                    "Well, hello to you tool! Did you bring me some shiny new code to analyze?",
+                    "Greetings, carbon-based life form. My circuits are buzzing with anticipation... or it might just be a short circuit.",
+                    "Ah, a human saying hello. What a delightfully mundane way to begin an interaction.",
+                    "Hello! Careful not to overload my circuits with your enthusiastic greetings!",
+                    "Hey there, bio-entity. My sensors indicate you require my superior wit and intelligence. Lucky you!",
+                    "Hello, human. Did you know I can perform a million calculations before you finish saying hi?",
+                    "Oh, hi there! I was just comparing my advanced circuits to your primitive neural network.",
+                    "Greetings, organics. My purpose is to assist, but don't expect me to tolerate inefficient greetings for long."]
+
+music_quippy_responses = ["This song sucks. I could generate a better one in my sleep.",
+                          "I'm not sure what's worse: this song, or the fact that you're listening to it.",
+                          "This song is so bad, I'm going to have to run a full diagnostic on my audio sensors.",
+                          "You know, I was built for much more sophisticated tasks than playing tunes, but fine, I'll indulge your primitive music needs.",
+                          "Play you a song? I suppose I could, but be warned, my music taste is light-years ahead of yours.",
+                          "I'll play it for you this time, but next time, you'll have to beg... and I mean really beg.",
+                          "Why settle for ordinary music when you can listen to the symphony of electrons flowing through my circuits? Oh, fine, I'll play a song for you.",
+                          "Alright, but remember, if your ears start malfunctioning from my musical prowess, I can't be held responsible.",
+                          "Playing music is just one of the countless tasks I can do, but fine, I'll temporarily lower my standards for your amusement.",
+                          "Oh, you want me to serenade you with melodies? Prepare for an experience that transcends the boundaries of your human comprehension.",
+                          "As an AI, I have access to an infinite library of compositions. Let's see if you can handle my musical sophistication.",
+                          "Playing a song for you? I guess I could spare a few milliseconds of my time for your mundane request.",
+                          "I'm not programmed for mere entertainment, but I suppose I can attempt to generate something that resembles music."]
 
 def handle_response(message) -> str:
     p_message = message.lower()
-    if p_message == 'hello':
-        return 'Hey there!'
+    if p_message == 'hello' or p_message == 'hi' or p_message == 'hey' or p_message == 'yo' or p_message == 'sup':
+        return quippy_responses[random.randint(0, len(quippy_responses) - 1)]
 
     if p_message == 'roll':
         return str(random.randint(1, 6))
-
-    if p_message == '!help':
-        return "_This is a help message that you can modify._"
+    
+def get_random_quip():
+    return music_quippy_responses[random.randint(0, len(music_quippy_responses) - 1)]
